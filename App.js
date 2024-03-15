85e431c0d38b7318fe9718531748fa3f2790ea72
João Pedro Araújo Teixeira <116459774+JoaoPedro999@users.noreply.github.com>
2024-03-15 15:27:52 -0300

    commit

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { View } from "react-native";
+import styles from "./src/styles/styles";
+import TouristLocation from "./src/components/TouristLocation";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TouristLocation
+        name="Cristo Redentor"
+        adress="Parque Nacional da Tijuca - Alto da Boa Vista, Rio de Janeiro - RJ"
+        description="Uma das 7 maravilhas do mundo moderno"
+        imagesource={require("./src/Images/Cristo-DePaula-2.jpg")}
+      />
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
